876e3e6e9f925da6a9a0906387f9b9d011adeae6
Michelle Kwan <michellekwan@Michelles-MacBook-Pro.local>
2022-03-19 19:10:17 -0700

    Test

--- Clue.py
+++ Clue.py
@@ -7,7 +7,8 @@ class Clue:
 
 player1 = Playergame name)
 
-
+gem install rails --version 7.0.2.3s
 #card format = [room, weapon, suspect]
 #player will make accusation, and deck class will check
 #player will pass other players a suggestion.
+#newrlkejrlwkerjwlekrjqweklrqjwe test test test
