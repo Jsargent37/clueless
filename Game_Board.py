bf0c1141a875a9b7875ab03d05f54e731743ec0e
Jsargent37 <101677786+Jsargent37@users.noreply.github.com>
2022-03-21 12:11:22 -0500

    Changed Game_Board.py to new setup we discussed in meeting on 03-17-2022

--- Game_Board.py
+++ Game_Board.py
@@ -1,65 +1,44 @@
-from Room import Rm
-
 class Board:
     def __init__(self):
-        self.study = Rm(False, "Study")
-        self.hallwaySH = Rm(True, "Hallway to Study or Hall")
-        self.hall = Rm(False, "Hall")
-        self.hallwayHL = Rm(True, "Hallway to Hall or Lounge")
-        self.lounge = Rm(False, "Lounge")
-        self.hallwaySL = Rm(True, "Hallway to Study or Library")
-        self.hallwayHB = Rm(True, "Hallway to Hall or Billiard Room")
-        self.hallwayLD = Rm(True, "Hallway to Lounge or Dining Room")
-        self.library = Rm(False, "Library")
-        self.hallwayLB = Room(True, "Hallway to Library or Billiard Room")
-        self.billiard_room = Room(False, "Billiard Room")
-        self.hallwayBD = Room(True, "Hallway to Billiard Room or Dining Room")
-        self.dining_room = Room(False, "Dining Room")
-        self.hallwayLC = Room(True, "Hallway to Library or Conservatory")
-        self.hallwayBB = Room(True, "Hallway to Billiard Room or Ballroom")
-        self.hallwayDK = Room(True, "Hallway to Dining Room or Kitchen")
-        self.conservatory = Room(False, "Conservatory")
-        self.hallwayCB = Room(True, "Hallway to Conservatory or Ballroom")
-        self.ballroom = Room(False, "Ballroom")
-        self.hallwayBK = Room(True, "Hallway to Ballroom or Kitchen")
-        self.kitchen = Room(False, "Kitchen")
-    
-        self.study.setRoomConnections([0, self.hallwaySL, self.hallwaySH, 0, self.kitchen])
-        self.hallwaySH.setRoomConnections([0, 0, self.hall, self.study, 0])
-        self.hall.setRoomConnections([0, self.hallwayHB, self.hallwayHL, self.hallwaySH, 0])
-        self.hallwayHL.setRoomConnections([0, 0, self.lounge, self.hall, 0])
-        self.lounge.setRoomConnections([ 0, self.hallwayLD, 0, self.hallwayHL, self.conservatory])
-        self.hallwaySL.setRoomConnections([ self.study, self.library, 0, 0, 0])
-        self.hallwayHB.setRoomConnections([ self.hall, self.billiard_room, 0, 0, 0])
-        self.hallwayLD.setRoomConnections([ self.lounge, self.dining_room, 0, 0, 0])
-        self.library.setRoomConnections([self.hallwaySL, self.hallwayLC, self.hallwayLB, 0, 0])
-        self.hallwayLB.setRoomConnections([0, 0, self.billiard_room, self.library, 0])
-        self.billiard_room.setRoomConnections([ self.hallwayHB, self.hallwayBB, self.hallwayBD, self.hallwayLB, 0])
-        self.hallwayBD.setRoomConnections([ 0, 0, self.dining_room, self.billiard_room, 0])
-        self.dining_room.setRoomConnections([self.hallwayLD, self.hallwayDK, 0, self.hallwayBD, 0])
-        self.hallwayLC.setRoomConnections([self.library, self.conservatory, 0, 0, 0])
-        self.hallwayBB.setRoomConnections([self.billiard_room, self.ballroom, 0, 0, 0])
-        self.hallwayDK.setRoomConnections([ self.dining_room, self.kitchen, 0, 0, 0])
-        self.conservatory.setRoomConnections([self.hallwayLC, 0, self.hallwayCB, 0, self.lounge])
-        self.hallwayCB.setRoomConnections([0, 0, self.ballroom, self.conservatory, 0])
-        self.ballroom.setRoomConnections([self.hallwayBB, 0, self.hallwayBK, self.hallwayCB, 0])
-        self.hallwayBK.setRoomConnections([0, 0, self.kitchen, self.ballroom, 0])
-        self.kitchen.setRoomConnections([ self.hallwayDK, 0, 0, self.hallwayBK, self.study])
+        self.gameBoard = [[2, 1, 2, 1, 2],
+        [1, 0, 1, 0, 1],
+        [2, 1, 2, 1, 2],
+        [1, 0, 1, 0, 1],
+        [2, 1, 2, 1, 2]]
 
-    def movesForPlayer(self, playerPosition: Room):
-        possibleMoves = []
-        for i in range(len(playerPosition.moves)):
-            if playerPosition.moves[i] != 0:
-                if playerPosition.moves[i].canPlayerEnterRoom() == True:
-                    possibleMoves.append(playerPosition.moves[i])
-        
-        return possibleMoves
+        self.roomsCor = {(0,0):"Study", (0,1):"Hallway", (0,2):"Hall", (0,3):"Hallway", (0,4):"Lounge",
+        (1,0):"Hallway", (1,2):"Hallway", (1,4):"Hallway", (2,0):"Library", (2,1):"Hallway", (2,2):"Billiard Room",
+        (2,3):"Hallway", (2,4):"Dining Room", (3,0): "Hallway", (3,2): "Hallway", (3,4):"Hallway",
+        (4,0):"Conservatory", (4,1):"Hallway", (4,2):"Ballroom", (4,3):"Hallway", (4,4):"Kitchen"}
 
-def main():
-    x = Board()
-    playerMoves = x.movesForPlayer(x.billiard_room)
+    def whereToMoveTo(self, cord):
+        i, j = cord[0], cord[1]
+        movesCords = []
+        if i == 0 and j == 0:
+            movesCords.append((4,4))
+        elif i == 4 and j == 0:
+            movesCords.append((0,4))
+        elif i == 0 and j == 4:
+            movesCords.append((4,0))
+        elif i == 4 and j == 4:
+            movesCords.append((0,0))
+        if self.gameBoard[i-1][j] == 1 or self.gameBoard[i-1][j] == 2:
+            movesCords.append((i-1,j))
+        if self.gameBoard[i][j-1] == 1 or self.gameBoard[i][j-1] == 2:
+            movesCords.append((i,j-1))
+        if self.gameBoard[i+1][j] == 1 or self.gameBoard[i+1][j] == 2:
+            movesCords.append((i+1,j))
+        if self.gameBoard[i][j+1] == 1 or self.gameBoard[i][j+1] == 2:
+            movesCords.append((i,j+1))
+        return movesCords
 
-    for i in playerMoves:
-        print(i.name)
+    def blockAndUnblockHallway(self, cord):
+        i, j = cord[0], cord[1]
+        if self.gameBoard[i][j] == 1:
+            self.gameBoard = -1
+        elif self.gameBoard[i][j] == -1:
+            self.gameBoard = 1
 
-main()
+    def returnRoom(self, cord):
+        cord1 = (cord[0], cord[1])
+        return self.roomsCor.get(cord1)
