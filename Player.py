7b79581ae271958b0b07b843329bfefd43f53309
michelle2662 <wkwan1122@gmail.com>
2022-03-22 13:00:10 -0700

    Updated

--- Player.py
+++ Player.py
@@ -1,34 +1,42 @@
+'''
+3/21/2022
+This class represents the player. It initalizes the player with a name, their hand, and the starting position. 
+setPlayerHand function takes player's new hand in and set it to players hand
+checkSuggestion function takes a deck object and calls guess to to see if suggestion is accurate
+setCurrentPosition function takes the new position of where player is and set it to position
+getCurrentPosition function returns the player's position
+'''
+
+#from Deck import Deck
+
 class Player:
     
-    def __init__(self,name, hand, startingPos):
+    #initalize player with name, hand and their starting position
+    def __init__(self, name, hand, startingPos):
         self.name = name
-        self.deck = hand
+        self.playerHand = hand
+        self.position = startingPos
 
+    #set player new hand
     def setPlayerHand(self,hand):
-        self.deck = hand
+        self.playerHand = hand
         
-    def makeSuggestion(self,deck):
-
-        suggestion_weapon = deck[0]
-        suggestion_room = deck[1]
-        suggestion_suspect = deck[2]
-        suggestion = [suggestion_suspect, suggestion_room, suggestion_weapon]
-        return self.suggestion
+    #call deck class to check if accusation is true
+    def checkSuggestion(self, suggestedCards):
+        return suggestedCards.guess(suggestedCards)
     
+    #return player name
     def getName(self):
         return self.name
     
+    #set current position
     def setCurrentPosition(self,pos):
         self.position = pos
     
+    #return current position
     def getCurrentPosition(self):
         return self.position
-    
-    def setStartingPosition(self,startingPos):
-        self.startingPosition = startingPos
-    
-    def getStartingPosition(self):
-        return self.startingPosition
+
     
      
     
